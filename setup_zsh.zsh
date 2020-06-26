ad0ee5b1b333e47ace0102cbe16be78263ac2d0b
Patrick McDonald <66495007+eieioxyz@users.noreply.github.com>
2020-06-26 09:41:51 -0400

    Redirect standard output to the blackhole

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -5,7 +5,7 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 # Installation unnecessary; it's in the Brewfile.
 
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/usr/local/bin/zsh'
