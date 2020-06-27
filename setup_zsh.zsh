fb980cf086d0f43b658e7a793e1ce8da22747a79
Patrick McDonald <66495007+eieioxyz@users.noreply.github.com>
2020-06-27 12:17:02 -0400

    Prevent superfluous commands with conditionals

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,8 +4,18 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation unnecessary; it's in the Brewfile.
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+# https://stackoverflow.com/a/4749368/1341838
+if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
+  echo '/usr/local/bin/zsh already exists in /etc/shells'
+else
+  # echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
-echo "Enter user password to change login shell"
-chsh -s '/usr/local/bin/zsh'
+
+if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
+  echo '$SHELL is already /usr/local/bin/zsh'
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/usr/local/bin/zsh'
+fi
