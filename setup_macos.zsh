cc10de9eab33f4023d33cef6bad7349e95b6e7bd
Patrick McDonald <66495007+eieioxyz@users.noreply.github.com>
2020-08-07 15:42:54 -0400

    Add setup_macos.zsh and matching Dotbot command with Finder settings
    Related: #15

---
+++ setup_macos.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting macOS Setup >>>\n"
+
+# Finder > View > Show Path Bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder > Preferences > General > New Finder windows show:
+defaults write com.apple.finder NewWindowTarget -string 'PfLo'
+defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/.dotfiles"
+
+
