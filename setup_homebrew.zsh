45005aad514673d92df2536a1403cad7a4bf9fd6
iamtodor <todor.ilya@gmail.com>
2022-09-28 12:05:00 +0200

    Update setup_homebrew.zsh
    update brew installation as its recommender on the official page https://brew.sh/

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -6,7 +6,7 @@ if exists brew; then
   echo "brew exists, skipping install"
 else
   echo "brew doesn't exist, continuing with install"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 
